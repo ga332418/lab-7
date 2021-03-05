63a321e027c84fd8be5503d73f3219e863cf9f26
ga332418 <77756046+ga332418@users.noreply.github.com>
2021-03-05 15:21:24 -0500

    Add files via upload

---
+++ lab7.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Gabriel Avink
+ *        Date: 1/27/2021
+ * Description: git lab 2
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+cout << "Hello Git!!" << endl;
+    /*add code*/
+    return 0;
+}// main
